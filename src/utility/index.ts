f74e6a9f4057875f6665c88d41fc2a6d13a7e19f
Steele Scott <60533996+steele123@users.noreply.github.com>
2023-11-24 11:16:02 -0500

    info endpoint

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -21,3 +21,15 @@ utilityRouter.get("/info", async (c) => {
         tlsVersion: c.req.header("CF-IPCountry") ?? "Unknown TLS Version",
     })
 })
+
+utilityRouter.get("/me", async (c) => {
+    return c.json({
+        ip: c.req.header("CF-Connecting-IP") ?? "Unknown IP",
+        ua: c.req.header("User-Agent") ?? "Unknown User-Agent",
+        country: c.req.header("CF-IPCountry") ?? "Unknown Country",
+        asn: c.req.header("CF-Connecting-ASN") ?? "Unknown ASN",
+        colo: c.req.header("CF-IPCountry") ?? "Unknown Colo",
+        tls: c.req.header("CF-IPCountry") ?? "Unknown TLS",
+        tlsVersion: c.req.header("CF-IPCountry") ?? "Unknown TLS Version",
+    })
+})
