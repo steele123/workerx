3d455f92d7a6b363eef11511cc4a87191270c61d
Steele Scott <60533996+steele123@users.noreply.github.com>
2023-11-24 11:22:38 -0500

    fix info

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,36 +1,51 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
 
 export const utilityRouter = new Hono();
 
-utilityRouter.get("/ip", async (c) => {
-    return c.text(c.req.header("CF-Connecting-IP") ?? "Unknown IP")
-})
+utilityRouter.get('/ip', async (c) => {
+	return c.text(c.req.header('CF-Connecting-IP') ?? 'Unknown IP');
+});
 
-utilityRouter.get("/ua", async (c) => {
-    return c.text(c.req.header("User-Agent") ?? "Unknown User-Agent")
-})
+utilityRouter.get('/ua', async (c) => {
+	return c.text(c.req.header('User-Agent') ?? 'Unknown User-Agent');
+});
 
-utilityRouter.get("/info", async (c) => {
-    return c.json({
-        ip: c.req.header("CF-Connecting-IP") ?? "Unknown IP",
-        ua: c.req.header("User-Agent") ?? "Unknown User-Agent",
-        country: c.req.header("CF-IPCountry") ?? "Unknown Country",
-        asn: c.req.header("CF-Connecting-ASN") ?? "Unknown ASN",
-        colo: c.req.header("CF-IPCountry") ?? "Unknown Colo",
-        tls: c.req.header("CF-IPCountry") ?? "Unknown TLS",
-        tlsVersion: c.req.header("CF-IPCountry") ?? "Unknown TLS Version",
-    })
-})
+utilityRouter.get('/info', async (c) => {
+	return c.json({
+		ip: c.req.header('CF-Connecting-IP') ?? 'Unknown IP',
+		ua: c.req.header('User-Agent') ?? 'Unknown User-Agent',
+		country: c.req.raw.cf?.country ?? 'Unknown Country',
+		asn: c.req.raw.cf?.asOrganization ?? 'Unknown ASN',
+        timezone: c.req.raw.cf?.timezone ?? 'Unknown Timezone',
+        city: c.req.raw.cf?.city ?? 'Unknown City',
+        postalCode: c.req.raw.cf?.postalCode ?? 'Unknown Postal Code',
+        region: c.req.raw.cf?.region ?? 'Unknown Region',
+        lat: c.req.raw.cf?.latitude ?? 'Unknown Latitude',
+        lon: c.req.raw.cf?.longitude ?? 'Unknown Longitude',
+		colo: c.req.raw.cf?.colo ?? 'Unknown Colo',
+        http: c.req.raw.cf?.httpProtocol ?? 'Unknown HTTP Protocol',
+		tls: c.req.raw.cf?.tlsCipher ?? 'Unknown TLS',
+		tlsVersion: c.req.raw.cf?.tlsVersion ?? 'Unknown TLS Version',
+    
+	});
+});
 
-utilityRouter.get("/me", async (c) => {
-    let text = ""
-    text += `IP: ${c.req.header("CF-Connecting-IP") ?? "Unknown IP"}\n`
-    text += `User-Agent: ${c.req.header("User-Agent") ?? "Unknown User-Agent"}\n`
-    text += `Country: ${c.req.header("CF-IPCountry") ?? "Unknown Country"}\n`
-    text += `ASN: ${c.req.header("CF-Connecting-ASN") ?? "Unknown ASN"}\n`
-    text += `Colo: ${c.req.header("CF-IPCountry") ?? "Unknown Colo"}\n`
-    text += `TLS: ${c.req.header("CF-IPCountry") ?? "Unknown TLS"}\n`
-    text += `TLS Version: ${c.req.header("CF-IPCountry") ?? "Unknown TLS Version"}\n`
+utilityRouter.get('/me', async (c) => {
+	let text = '';
+	text += `IP: ${c.req.header('CF-Connecting-IP') ?? 'Unknown IP'}\n`;
+	text += `User-Agent: ${c.req.header('User-Agent') ?? 'Unknown User-Agent'}\n`;
+	text += `Country: ${c.req.raw.cf?.country ?? 'Unknown Country'}\n`;
+	text += `ASN: ${c.req.raw.cf?.asOrganization ?? 'Unknown ASN'}\n`;
+	text += `Timezone: ${c.req.raw.cf?.timezone ?? 'Unknown Timezone'}\n`;
+	text += `City: ${c.req.raw.cf?.city ?? 'Unknown City'}\n`;
+	text += `Postal Code: ${c.req.raw.cf?.postalCode ?? 'Unknown Postal Code'}\n`;
+	text += `Region: ${c.req.raw.cf?.region ?? 'Unknown Region'}\n`;
+	text += `Latitude: ${c.req.raw.cf?.latitude ?? 'Unknown Latitude'}\n`;
+	text += `Longitude: ${c.req.raw.cf?.longitude ?? 'Unknown Longitude'}\n`;
+	text += `Colo: ${c.req.raw.cf?.colo ?? 'Unknown Colo'}\n`;
+	text += `HTTP Protocol: ${c.req.raw.cf?.httpProtocol ?? 'Unknown HTTP Protocol'}\n`;
+	text += `TLS: ${c.req.raw.cf?.tlsCipher ?? 'Unknown TLS'}\n`;
+	text += `TLS Version: ${c.req.raw.cf?.tlsVersion ?? 'Unknown TLS Version'}\n`;
 
-    return c.text(text)
-})
+	return c.text(text);
+});
