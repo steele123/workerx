2caf6a4c2f6899a4da114a4f1e16e6864735335f
Steele Scott <60533996+steele123@users.noreply.github.com>
2023-11-24 11:05:54 -0500

    add more utilities about req info

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono';
 import { shareRouter } from './sharex';
+import { utilityRouter } from './utility';
 
 const app = new Hono();
 
 app.route("", shareRouter)
+
+app.route("", utilityRouter)
 
 export default app; // for Cloudflare Workers or Bun
---
+++ src/utility/index.ts
@@ -0,0 +1,23 @@
+import { Hono } from "hono";
+
+export const utilityRouter = new Hono();
+
+utilityRouter.get("/ip", async (c) => {
+    return c.text(c.req.header("CF-Connecting-IP") ?? "Unknown IP")
+})
+
+utilityRouter.get("/ua", async (c) => {
+    return c.text(c.req.header("User-Agent") ?? "Unknown User-Agent")
+})
+
+utilityRouter.get("/info", async (c) => {
+    return c.json({
+        ip: c.req.header("CF-Connecting-IP") ?? "Unknown IP",
+        ua: c.req.header("User-Agent") ?? "Unknown User-Agent",
+        country: c.req.header("CF-IPCountry") ?? "Unknown Country",
+        asn: c.req.header("CF-Connecting-ASN") ?? "Unknown ASN",
+        colo: c.req.header("CF-IPCountry") ?? "Unknown Colo",
+        tls: c.req.header("CF-IPCountry") ?? "Unknown TLS",
+        tlsVersion: c.req.header("CF-IPCountry") ?? "Unknown TLS Version",
+    })
+})
