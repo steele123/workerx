026477187bc5ddb6f570aac4da7bbf709615b86e
Steele Scott <60533996+steele123@users.noreply.github.com>
2023-11-24 11:17:13 -0500

    add text endpoint

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -23,13 +23,14 @@ utilityRouter.get("/info", async (c) => {
 })
 
 utilityRouter.get("/me", async (c) => {
-    return c.json({
-        ip: c.req.header("CF-Connecting-IP") ?? "Unknown IP",
-        ua: c.req.header("User-Agent") ?? "Unknown User-Agent",
-        country: c.req.header("CF-IPCountry") ?? "Unknown Country",
-        asn: c.req.header("CF-Connecting-ASN") ?? "Unknown ASN",
-        colo: c.req.header("CF-IPCountry") ?? "Unknown Colo",
-        tls: c.req.header("CF-IPCountry") ?? "Unknown TLS",
-        tlsVersion: c.req.header("CF-IPCountry") ?? "Unknown TLS Version",
-    })
+    let text = ""
+    text += `IP: ${c.req.header("CF-Connecting-IP") ?? "Unknown IP"}\n`
+    text += `User-Agent: ${c.req.header("User-Agent") ?? "Unknown User-Agent"}\n`
+    text += `Country: ${c.req.header("CF-IPCountry") ?? "Unknown Country"}\n`
+    text += `ASN: ${c.req.header("CF-Connecting-ASN") ?? "Unknown ASN"}\n`
+    text += `Colo: ${c.req.header("CF-IPCountry") ?? "Unknown Colo"}\n`
+    text += `TLS: ${c.req.header("CF-IPCountry") ?? "Unknown TLS"}\n`
+    text += `TLS Version: ${c.req.header("CF-IPCountry") ?? "Unknown TLS Version"}\n`
+
+    return c.text(text)
 })
